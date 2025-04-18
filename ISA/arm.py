9879205adbb0ec9246fa13392ef486b932906123
Jungle Schmidt <caesarhao@126.com>
2025-04-18 11:08:17 +0200

    Create arm.py

---
+++ ISA/arm.py
@@ -0,0 +1,23 @@
+from ctypes import *
+"""
+types defined in ctypes : 
+  c_bool, 
+  c_char : char, 
+  c_byte : char, 
+  c_ubyte : unsigned char
+  c_short : short
+  c_ushort : unsigned short
+  c_int : int
+  c_uinit : unsigned int
+  c_long : long
+  c_ulong : unsigned long
+  c_longlong : long long
+  c_ulonglong : unsigned long long
+  c_size_t : size_t
+  c_float : float
+  c_double : double
+  c_longdouble : long double
+  c_char_p : char *
+  c_void_p : void *
+  
+"""
