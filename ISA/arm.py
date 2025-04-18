ed4582ba3cb4103c525738ce3f95c8620a1b396f
Jungle Schmidt <caesarhao@126.com>
2025-04-18 11:22:48 +0200

    Update arm.py

--- ISA/arm.py
+++ ISA/arm.py
@@ -1,6 +1,6 @@
 from ctypes import *
 """
-types defined in ctypes : 
+types defined in ctypes (they are all classes. create instance like ci = c_int(25), get value ci.value, change value ci.value=20): 
   c_bool, 
   c_char : char, 
   c_byte : char, 
@@ -19,5 +19,16 @@ types defined in ctypes :
   c_longdouble : long double
   c_char_p : char *
   c_void_p : void *
-  
+
+  Structure and Union, _fields_ must be defined.
+    class POINT(Structure):
+        _fields_ = [("x", c_int),
+                ("y", c_int)]
+    point = POINT(10, 20)
+    print(point.x, point.y)
+
+    class RECT(Structure):
+        _fields_ = [("upperleft", POINT),
+                 ("lowerright", POINT)]
+
 """
